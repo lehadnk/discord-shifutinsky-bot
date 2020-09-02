1b6eb71abaa6b1ee103546deb02c75de8f91a1b4
Alexey Zauzin <lehadnk@gmail.com>
2020-09-02 23:13:27 +0300

    Attempting to fix the bug

--- src/PermittedChannelList.ts
+++ src/PermittedChannelList.ts
@@ -3,7 +3,7 @@ import {SqliteDbAdapter} from "./SqliteDbAdapter";
 export default class PermittedChannelList {
     private db: SqliteDbAdapter;
 
-    private permittedChannels = new Map<string, void>();
+    private permittedChannels = new Map<string, string>();
 
     constructor(db: SqliteDbAdapter) {
         this.db = db;
@@ -17,7 +17,7 @@ export default class PermittedChannelList {
 
     addPermission(channel_id: string, author_id: string, author_name: string)
     {
-        this.permittedChannels.set(channel_id);
+        this.permittedChannels.set(channel_id, channel_id);
         this.db.run("INSERT INTO permitted_channels(channel_id, author_id, author_name) VALUES (?1, ?2, ?3)", {
             1: channel_id,
             2:author_id,
@@ -29,7 +29,7 @@ export default class PermittedChannelList {
     {
         let data = await this.db.all("SELECT * FROM permitted_channels");
         data.forEach((row) => {
-            this.permittedChannels.set(row.channel_id);
+            this.permittedChannels.set(row.channel_id, row.channel_id);
             console.log(row.channel_id);
         })
     }
