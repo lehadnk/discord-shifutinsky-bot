22bfdc864fb175880447fc409967e714ec2a8898
Alexey Zauzin <lehadnk@gmail.com>
2020-09-02 23:25:50 +0300

    Fixed a bug

--- src/PermittedChannelList.ts
+++ src/PermittedChannelList.ts
@@ -17,7 +17,7 @@ export default class PermittedChannelList {
 
     addPermission(channel_id: string, author_id: string, author_name: string)
     {
-        console.log('Adding new permission: ' + channel_id + ' - ' + author_name + '#' + author_name);
+        console.log('Adding new permission: ' + channel_id + ' - ' + author_id + '/' + author_name);
         this.permittedChannels.set(channel_id, channel_id);
         this.db.run("INSERT INTO permitted_channels(channel_id, author_id, author_name) VALUES (?1, ?2, ?3)", {
             1: channel_id,
@@ -28,7 +28,7 @@ export default class PermittedChannelList {
 
     async loadPermissions()
     {
-        let data = await this.db.all("SELECT * FROM permitted_channels");
+        let data = await this.db.all("SELECT channel_id FROM permitted_channels");
         data.forEach((row) => {
             this.permittedChannels.set(row.channel_id, row.channel_id);
             console.log(row.channel_id);
