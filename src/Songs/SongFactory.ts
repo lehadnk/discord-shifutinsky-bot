45a585d648e460b2fea030a7b7484a1858cd93ea
Alexey Zauzin <lehadnk@gmail.com>
2021-02-23 00:29:52 +0300

    Added 23 february

--- src/Songs/SongFactory.ts
+++ src/Songs/SongFactory.ts
@@ -1,11 +1,16 @@
 import {SongInterface} from "./SongInterface";
 import {ThirdSeptember} from "./ThirdSeptember";
 import {TwentyThirdSeptember} from "./TwentyThirdSeptember";
+import {TwentyThirdFebruary} from "./TwentyThirdFebruary";
 
 export class SongFactory {
     static getTodaySong(): SongInterface
     {
         let date = new Date();
+
+        if ((date.getMonth() === 1) && (date.getDate() == 23)) {
+            return new TwentyThirdFebruary();
+        }
 
         // Fucking amazing world of JavaScript - 8 means September because fuck you that's why
         if (date.getMonth() != 8) {
